24f7a9af812c55a23b763fae5112523a28239139
J0EL-1SSAC <joelissac55@gmail.com>
2026-03-08 16:47:14 +0530

    Updating homepage js

---
+++ page.js
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <main style={{padding:40}}>
+      <h1>DFIR Insider Threat Detection System</h1>
+
+      <p>
+        AI powered SOC dashboard detecting suspicious activity
+        and insider threats.
+      </p>
+
+      <br/>
+
+      <a href="/employee">
+        <button>Employee Portal</button>
+      </a>
+
+      <br/><br/>
+
+      <a href="/admin">
+        <button>Admin SOC Dashboard</button>
+      </a>
+
+    </main>
+  )
+}
